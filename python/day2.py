f2eeb22c3413eeeddcc8ceb5d0a4896214c93b0f
Rofiqunnab <rofiqunnabironobi228265@gmail.com>
2026-01-03 01:39:30 +0600

    day 2nd work

---
+++ python/day2.py
@@ -0,0 +1,18 @@
+# variable 
+name ="ronobi"
+print(name)
+age1 =200
+age2=30
+print ("befor",age1)
+age2 = age1
+print("after",age1)
+#input taking form user
+yourname=input("Enter your name :")
+print("lets break the chlange and show " ,yourname + " who you are")
+yourtime= input ("terget in 2026 finised in:")
+print ("insawallha it will be work before the " ,yourtime +" done")
+#oprator & oprarent  & opration expression
+x=5 
+y=10
+z=x+y
+print(z)
