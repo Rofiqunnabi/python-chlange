df1749adc2b2128a6a90eab0b0a6842062fd4c64
Rofiqunnab <rofiqunnabironobi228265@gmail.com>
2026-01-06 01:05:03 +0600

    created mini project Great calculator

---
+++ python/project list/Great_calculator.py
@@ -0,0 +1,17 @@
+mark=int(input("enter your mark :"))
+if(mark>=90):
+    print("your marked great is A+")   
+elif(mark>=80):
+    print("your marked great is A")   
+elif(mark>=70):
+    print("your marked great is B+")   
+elif(mark>=65):
+    print("your marked great is B")   
+elif(mark>=60):
+    print("your marked great is C+")   
+elif(mark>=50):
+    print("your marked great is C")  
+else:
+    print("you need to focus on study") 
+
+print("Good Luck for your result")
